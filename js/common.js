b30fabd7b8a5556bbe7cd874783aefa19638c1ee
Hiroki Tatara <tatara0862@gmail.com>
2021-09-06 19:52:59 +0900

    ページトップボタンの実装

--- js/common.js
+++ js/common.js
@@ -55,3 +55,22 @@ tabLabels.forEach((clickedLabel) => {
     document.getElementById(clickedLabel.dataset.id).classList.add('active')
   })
 })
+
+// ページトップボタン
+const pageTop = document.getElementById('page_top')
+
+pageTop.addEventListener('click', () => {
+  window.scroll({
+    top: 0,
+    behavior: 'smooth'
+  })
+})
+
+const pageTopShow = document.querySelector('.page_top')
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    pageTopShow.classList.add('show')
+  } else {
+    pageTopShow.classList.remove('show')
+  }
+})
